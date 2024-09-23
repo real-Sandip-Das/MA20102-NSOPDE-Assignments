f6768122abf49a7660725901578f25b87bdc9a95
real-Sandip-Das <sd13535@outlook.com>
2024-09-23 23:22:03 +0530

    Added argument "decimals" to functions of existing methods for specifying decimal places to round up

--- Euler_single_step_methods.py
+++ Euler_single_step_methods.py
@@ -3,19 +3,19 @@ from Newton_Raphson import newton_raphson
 
 ## Euler's explicit method
 
-def euler_explicit(f, a: np.float32, b: np.float32, y_0: np.float32, N: np.uint32):
+def euler_explicit(f, a: np.float32, b: np.float32, y_0: np.float32, N: np.uint32, decimals: int = 5):
     h = (b - a)/N
     x_n = np.array([a + n*h for n in range(N+1)], dtype=np.float32)
     y_n = np.zeros((N+1,), dtype=np.float32)
     y_n[0] = y_0
     for n in range(N):
-        y_n[n + 1] = np.round(y_n[n] + h * f(x_n[n], y_n[n]), 5)
+        y_n[n + 1] = np.round(y_n[n] + h * f(x_n[n], y_n[n]), decimals)
     
     return x_n, y_n
 
 ## Euler's implicit method
 
-def euler_implicit(f, f_y, a: np.float32, b: np.float32, y_0: np.float32, N: np.uint32):
+def euler_implicit(f, f_y, a: np.float32, b: np.float32, y_0: np.float32, N: np.uint32, decimals: int = 5):
     h = (b - a)/N
     x_n = np.array([a + n*h for n in range(N+1)], dtype=np.float32)
     y_n = np.zeros((N+1,), dtype=np.float32)
@@ -24,6 +24,6 @@ def euler_implicit(f, f_y, a: np.float32, b: np.float32, y_0: np.float32, N: np.
         g = lambda y: y - y_n[n] - h*f(x_n[n+1], y)
         g_y = lambda y: 1 - h*f_y(x_n[n+1], y)
         # get y_(n+1) by solving g(y_(n+1)) = 0
-        y_n[n+1] = np.round(newton_raphson(g, g_y, 0), 5)
+        y_n[n+1] = np.round(newton_raphson(g, g_y, 0), decimals)
     
     return x_n, y_n
--- Taylor_series_methods.py
+++ Taylor_series_methods.py
@@ -3,7 +3,7 @@ import math
 
 # Taylor series method
 
-def taylor_series_method(y_derivatives, a: np.float32, b: np.float32, y_0: np.float32, N: np.uint32):
+def taylor_series_method(y_derivatives, a: np.float32, b: np.float32, y_0: np.float32, N: np.uint32, decimals: int = 5):
     h = (b - a)/N
     x_n = np.array([a + n*h for n in range(N+1)], dtype=np.float32)
     y_n = np.zeros((N+1,), dtype=np.float32)
@@ -14,6 +14,6 @@ def taylor_series_method(y_derivatives, a: np.float32, b: np.float32, y_0: np.fl
 
     for n in range(N):
         series_sum = series_coefficients.T @ np.array([y_n[n] if (i == 0) else y_derivatives[i-1](x_n[n], y_n[n]) for i in range(len(y_derivatives) + 1)], dtype=np.float32)
-        y_n[n+1] = np.round(series_sum, 5)
+        y_n[n+1] = np.round(series_sum, decimals)
     
     return x_n, y_n
